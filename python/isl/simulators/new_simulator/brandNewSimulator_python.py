97cfdfd679a067297fbcd6a11c1be77cbd9e8bfc
Vakzze <rasmus@pinje.dk>
2022-03-08 10:09:26 +0100

    Update brandNewSimulator_python.py
    fix

--- python/isl/simulators/new_simulator/brandNewSimulator_python.py
+++ python/isl/simulators/new_simulator/brandNewSimulator_python.py
@@ -8,6 +8,6 @@ Created on Tue Mar  8 09:26:11 2022
 def simulator_func(value: float) -> float:
     return value
     
-def simulator_func_error(valaue: float) -> float:
+def simulator_func_error(value: float) -> float:
     bias = 0.1
     return value + bias
