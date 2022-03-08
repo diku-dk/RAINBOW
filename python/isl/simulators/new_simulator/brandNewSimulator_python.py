d82b6907f1ccab89539f49135c7bec5aae4741b6
Vakzze <rasmus@pinje.dk>
2022-03-08 10:07:30 +0100

    22: Adding new simulator with unittest
    22: Adding new simulator with unittest

---
+++ python/isl/simulators/new_simulator/brandNewSimulator_python.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 09:26:11 2022
+
+@author: Rasmus Netterstrøm
+"""
+
+def simulator_func(value: float) -> float:
+    return value
+    
+def simulator_func_error(valaue: float) -> float:
+    bias = 0.1
+    return value + bias
---
+++ python/isl/test/test_new_simulator/test_new_simulator.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 09:36:58 2022
+
+@author: Rasmus Netterstrøm
+"""
+
+import unittest
+import os
+import sys
+import numpy as np
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/../../")
+import isl.simulators.new_simulator.brandNewSimulator_python as bns
+import isl.test.test_common as utils
+
+
+class TestNewSimulator(unittest.TestCase):
+    def test_func_correct(self):
+        expected = 1
+        actual   = bns.simulator_func(1)
+        self.assertEqual(actual, expected)
+    
+    def test_func_array(self):
+        expected = np.array([1,2,3], dtype=np.float64)
+        actual   = np.array([
+            bns.simulator_func(1),
+            bns.simulator_func(2),
+            bns.simulator_func(3)
+            ])
+        
+        self.assertTrue(
+            utils.array_not_equal(actual, expected)
+            )
+        
+    def test_func_error(self):
+        expected = 1
+        actual   = bns.simulator_func_error(1)
+        self.assertEqual(actual, expected)
+        
