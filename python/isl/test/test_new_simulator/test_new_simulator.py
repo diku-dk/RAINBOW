c85a6997dcc1dac3484a2d426a0c50b5a2195f21
PadX <peidixu@qq.com>
2022-03-09 13:38:45 +0100

    Update test_new_simulator.py

--- python/isl/test/test_new_simulator/test_new_simulator.py
+++ python/isl/test/test_new_simulator/test_new_simulator.py
@@ -2,7 +2,7 @@ import unittest
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+  '/../../')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/../../')
 
 import isl.tutorial_test.test_code_peidi as peidi
 
@@ -13,11 +13,11 @@ class TestNewSimulator(unittest.TestCase):
 		actual = peidi.fabo(6)
 		self.assertEqual(actual, expected)
 		
-	def test_func_error(self):
-		expected = peidi.fabo(5)
-		# actual = bns.simulator_func_eoor(1)
-		actual = 2
-		self.assertEqual(actual, expected)
+	# def test_func_error(self):
+	# 	expected = peidi.fabo(5)
+	# 	# actual = bns.simulator_func_eoor(1)
+	# 	actual = 2
+	# 	self.assertEqual(actual, expected)
 
 	def test_fabo(self):
 		expected = 5
