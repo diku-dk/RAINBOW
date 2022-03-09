3a81215f9c0559872431df08f6abcbe9763d3400
PadX <peidixu@qq.com>
2022-03-09 12:02:14 +0100

    Update test_new_simulator.py

--- python/isl/test/test_new_simulator/test_new_simulator.py
+++ python/isl/test/test_new_simulator/test_new_simulator.py
@@ -8,14 +8,15 @@ import isl.tutorial_test.test_code_peidi as peidi
 
 class TestNewSimulator(unittest.TestCase):
 	def test_func_correct(self):
-		expected = 1
-		# actual = bns.simulator_func(1)
-		actual = 1
-		self.assertEqual(actual, expected)
-	def test_func_error(self):
-		expected = 1
+		expected = peidi.fabo(5) + peidi.fabo(4)
 		# actual = bns.simulator_func_eoor(1)
-		actual = 1
+		actual = peidi.fabo(6)
+		self.assertEqual(actual, expected)
+		
+	def test_func_error(self):
+		expected = peidi.fabo(5)
+		# actual = bns.simulator_func_eoor(1)
+		actual = 2
 		self.assertEqual(actual, expected)
 
 	def test_fabo(self):
