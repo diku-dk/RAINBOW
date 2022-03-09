2140023111fb2fc3da94a69a127a95cc4a3a8f9e
Kenny Erleben <kenny.erleben@gmail.com>
2022-03-09 12:03:20 +0100

    Update test_vandermonde.py

--- python/isl/test/test_vandermonde/test_vandermonde.py
+++ python/isl/test/test_vandermonde/test_vandermonde.py
@@ -12,20 +12,20 @@ class TestVandermonde(unittest.TestCase):
     def test_make_poly_string(self):
         expected = ""
         actual = V.make_poly_str(-1)
-        self.assertEquals(expected,actual, 'Negative degree polynomial test')
+        self.assertEqual(expected, actual, 'Negative degree polynomial test')
 
         expected = "1"
-        actual = V.make_poly_str(-1)
-        self.assertEquals(expected,actual, 'Zeroth order polynomial test')
+        actual = V.make_poly_str(0)
+        self.assertEqual(expected, actual, 'Zeroth order polynomial test')
 
         expected = "1 + x^1 + y^1"
         actual = V.make_poly_str(1)
-        self.assertEquals(expected,actual, 'First order polynomial test')
+        self.assertEqual(expected, actual, 'First order polynomial test')
 
         expected = "1 + x^1 + y^1 + x^2 + x^1y^1 + y^2"
         actual = V.make_poly_str(2)
-        self.assertEquals(expected,actual, 'Second order polynomial test')
+        self.assertEqual(expected, actual, 'Second order polynomial test')
 
         expected = "1 + x^1 + y^1 + x^2 + x^1y^1 + y^2 + x^3 + x^2y^1 + x^1y^2 + y^3"
         actual = V.make_poly_str(3)
-        self.assertEquals(expected,actual, 'Third order polynomial test')
+        self.assertEqual(expected, actual, 'Third order polynomial test')
