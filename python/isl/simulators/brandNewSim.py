7ce3d3bf0ba01776ee1403abb288d17a46e8d8bd
kasra-l1r <kasra@di.ku.dk>
2022-03-09 14:00:29 +0100

    added sim and test

---
+++ python/isl/simulators/brandNewSim.py
@@ -0,0 +1,12 @@
+"""
+Created on Wed Mar 9
+
+@author: l1r
+"""
+
+def simulator_func(value: float) -> float:
+    return value
+
+def simulator_func_error(value: float) -> float:
+    bias = 0.1
+    return value + bias
---
+++ python/isl/test/test_new_sim.py
@@ -0,0 +1,22 @@
+"""
+Created on Wed Mar 9
+
+@authro: l1r
+"""
+
+import unittest
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../")
+import isl.simulators.brandNewSim as bns
+
+class TestNewSim(unittest.TestCase):
+    def test_func_correct(self):
+        expected = 1
+        actual = bns.simulator_func(1)
+        self.assertEqual(actual, expected)
+
+    def test_func_error(self):
+        expected = 1
+        actual = bns.simulator_func(1)
+        self.assertEqual(actual, expected)
