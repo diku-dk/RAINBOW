2a22065edd647e59d2f55833fcb3111b30e3643b
Rasmus Netterstrøm <rasmus@pinje.dk>
2022-03-15 14:18:34 +0100

    41: Adds test to matrix3

--- python/isl/test/test_common.py
+++ python/isl/test/test_common.py
@@ -6,3 +6,12 @@ def array_equal(arr1, arr2):
 
 def array_not_equal(arr1, arr2):
     return np.any(np.not_equal(arr1,arr2))
+
+def psu_rand_array_gen(size, min_lim=10,max_lim=100):
+    rows, cols = size
+    length     = rows * cols
+    min_value  = 10 * np.random.random_sample()
+    max_value  = min_value * np.random.random_sample()  
+    values     = np.linspace(min_value, max_value, length)
+    np.random.shuffle(values)
+    return  values.reshape(size)
