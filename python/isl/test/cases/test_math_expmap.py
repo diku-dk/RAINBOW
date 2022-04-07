5d6f6c990c1fe1f44a35a134b9f1a9354e6bb699
Kenny Erleben <kenny@di.ku.dk>
2022-04-07 17:00:46 +0200

    Update test_math_expmap.py

--- python/isl/test/cases/test_math_expmap.py
+++ python/isl/test/cases/test_math_expmap.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/
 import isl.math.expmap as exm
 import isl.math.quaternion as Q
 import isl.math.vector3 as vec3
-import isl.test.testtools.Tools as Tools
+from isl.test.testtools import Tools
 
 
 class TestExomapAPI(unittest.TestCase):
