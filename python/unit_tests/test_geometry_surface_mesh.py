57542a8e43db3b2c04fa87d5ad751c59e9ca23c8
Kenny Erleben <kenny.erleben@gmail.com>
2024-04-30 14:53:10 +0200

    Update test_geometry_surface_mesh.py

--- python/unit_tests/test_geometry_surface_mesh.py
+++ python/unit_tests/test_geometry_surface_mesh.py
@@ -14,14 +14,11 @@ import rainbow.util.test_tools as TEST
 class TestConvexHull(unittest.TestCase):
 
     def test_tetrahedral_hull(self):
-
         points = []
-
         p5 = V3.make(0.1, 0.1, 0.1)
         points.append(p5)
         p6 = V3.make(0.2, 0.2, 0.2)
         points.append(p6)
-
         p1 = V3.make(1, 0, 0)
         p2 = V3.make(0, 1, 0)
         p3 = V3.make(0, 0, 1)
@@ -30,12 +27,10 @@ class TestConvexHull(unittest.TestCase):
         points.append(p2)
         points.append(p3)
         points.append(p4)
+        V_actual, T_actual = MESH.create_convex_hull(points)
+        V_expected = np.array([[1., 0., 0.],[0., 1., 0.],[0., 0., 1.],[0., 0., 0.]], dtype=np.float32)
+        T_expected = np.array([[2, 0, 1],[0, 3, 1],[3, 2, 1], [2, 3, 0]], dtype=int)
+        self.assertTrue(TEST.is_array_equal(V_actual, V_expected))
+        self.assertTrue(TEST.is_array_equal(T_actual, T_expected))
 
-        V, T = MESH.create_convex_hull(points)
-        print(V)
-        print(T)
-        #self.assertAlmostEqual(u1, w1)
-        #self.assertAlmostEqual(u2, w2)
-        #self.assertAlmostEqual(u3, w3)
-        #self.assertAlmostEqual(u4, w4)
 
