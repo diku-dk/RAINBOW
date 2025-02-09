99ec8aab977178fe389ae09a5a0a01d077713b0b
Sofus Tornbjerg <sofus.tornbjerg@gmail.com>
2025-02-09 11:29:44 +0100

    Refactor engine creation

--- rainbow/simulators/prox_rigid_bodies/scenes/create_engine.py
+++ rainbow/simulators/prox_rigid_bodies/scenes/create_engine.py
@@ -16,40 +16,17 @@ def create_engine(
     density: float = 1.0,
     material_name: str = "default",
     ):
-    logger = logging.getLogger("create_engine")
-    
-    def add_object(name: str, mesh: MESH.Mesh, position: np.ndarray, orientation: np.ndarray, body_type: str) -> str:
-        logger.info(f"Adding object {name}")
-        
-        shape_name = API.generate_unique_name(name + "_shape")
-        body_name = API.generate_unique_name(name + "_body")
-        API.create_shape(engine, shape_name, mesh)
-        API.create_rigid_body(engine, body_name)
-        API.connect_shape(engine, body_name, shape_name)
-        
-        API.set_position(engine, body_name, position, True)
-        API.set_orientation(engine, body_name, orientation, True)
-        API.set_body_type(engine, body_name, body_type)
-        API.set_body_material(engine, body_name, material_name)
-        API.set_mass_properties(engine, body_name, density)
-        return body_name
-    
     def add_gear(gear: GEAR.Gear, name: str):
-        return add_object(name, gear.mesh, gear.position, gear.orientation, "fixed" if gear.spec.is_internal else "free")
-    
-    def add_hinge(parent_name: str, child_name, axis: np.ndarray, origin: np.ndarray) -> str:
-        hinge_name = parent_name + "_" + child_name
-        API.create_hinge(engine, hinge_name)
-        API.set_hinge(
-            engine,
-            hinge_name,
-            parent_name,
-            child_name,
-            origin,
-            axis,
-            "world",
+        return API.add_object(
+            engine, 
+            name, 
+            gear.mesh, 
+            gear.position, 
+            gear.orientation, 
+            "fixed" if gear.spec.is_internal else "free",
+            material_name,
+            density,
         )
-        return hinge_name
     
     m = 0.1
     z_ring = 79
@@ -65,20 +42,20 @@ def create_engine(
     
     gear_factory = GEAR.GearFactory()
     
-    # Add first pair of gears
+    # Add gears
     ring1 = gear_factory.create_gear(ring_spec, face_width)
-    planet1 = gear_factory.create_gear(planet_spec, face_width)
-    
-    # Set the position and orientation of the planet gear
-    planet1.position = ring1.position - (ring_spec.rp - planet_spec.rp) * V3.j()
-    planet1.orientation = MATING.compute_gear_orientation(ring_spec, planet_spec, -np.pi/2)
-    
-    # Add second pair of gears
     ring2 = gear_factory.create_gear(ring_spec, face_width)
+    planet1 = gear_factory.create_gear(planet_spec, face_width)
     planet2 = gear_factory.create_gear(planet_spec, face_width)
     
-    # Set the position and orientation of the gears
+    # Set the position and orientation of the first pair
+    ring1.orientation = Q.Rz(-np.pi / 2)
+    planet1.position = ring1.position - (ring_spec.rp - planet_spec.rp) * V3.j()
+    planet1.orientation = ring1.orientation #MATING.compute_gear_orientation(ring_spec, planet_spec, -np.pi/2 - 2 * np.pi)
+    
+    # Set the position and orientation of the second pair
     ring2.position += gap_width * V3.k()
+    ring2.orientation = ring1.orientation
     planet2.position = planet1.position + gap_width * V3.k()
     planet2.orientation = planet1.orientation
     
@@ -87,53 +64,53 @@ def create_engine(
     crank_shaft_mesh = API.create_mesh(*MESH.create_cylinder(2 * ring_spec.rp, 2 * face_width, 16))
     crank_shaft_position = V3.make(0, 0, -2.1 * face_width)
     crank_shaft_orientation = Q.Rx(np.pi / 2)
-    crank_shaft_name = add_object("crank_shaft", crank_shaft_mesh, crank_shaft_position, crank_shaft_orientation, "free")
+    crank_shaft_name = API.add_object(engine, "crank_shaft", crank_shaft_mesh, crank_shaft_position, crank_shaft_orientation, material_name=material_name, density=density)
 
     # Add the gears to the engine
     ring1_name = add_gear(ring1, "ring1")
-    planet1_name = add_gear(planet1, "planet1")
     ring2_name = add_gear(ring2, "ring2")
-    planet2_name = add_gear(planet2, "planet2")
     
-    # Add interemdiate disks
+    # Add intermediate disks
     disk_radius = 0.5 * planet_spec.rp
-    disk1_mesh = API.create_mesh(*MESH.create_cylinder(disk_radius, 0.8 * face_width, 16))
-    disk2_mesh = API.create_mesh(*MESH.create_cylinder(disk_radius, 0.8 * face_width, 16))
-    disk1_position = planet1.position + face_width * V3.k() - eccentricity * V3.j()
-    disk2_position = disk1_position + 2 * face_width * V3.k()
-    disk_orientation = Q.Rx(np.pi / 2)
-    disk1_name = add_object("disk1", disk1_mesh, disk1_position, disk_orientation, "free")
-    disk2_name = add_object("disk2", disk2_mesh, disk2_position, disk_orientation, "free")
+    disk1_mesh = MESH.Mesh(*MESH.create_cylinder(disk_radius, 0.8 * face_width, 16))
+    disk2_mesh = MESH.Mesh(*MESH.create_cylinder(disk_radius, 0.8 * face_width, 16))
+    
+    disk1_offset = face_width * V3.k() + eccentricity * V3.i()
+    disk2_offset = -face_width * V3.k() + eccentricity * V3.i()
+    V1 = np.vstack([planet1.mesh.V, Q.rotate_array(Q.Rx(np.pi / 2), disk1_mesh.V) + disk1_offset])
+    V2 = np.vstack([planet2.mesh.V, Q.rotate_array(Q.Rx(np.pi / 2), disk2_mesh.V) + disk2_offset])
+    T1 = np.vstack([planet1.mesh.T, disk1_mesh.T + len(planet1.mesh.V)])
+    T2 = np.vstack([planet2.mesh.T, disk2_mesh.T + len(planet2.mesh.V)])
+    
+    planet1.mesh = API.create_mesh(V1, T1)
+    planet2.mesh = API.create_mesh(V2, T2)
+    
+    planet1_name = add_gear(planet1, "planet1")
+    planet2_name = add_gear(planet2, "planet2")
     
     # Add connecting rod
     connecting_rod_height = 2.5 * ring_spec.rp
     connecting_rod_mesh = API.create_mesh(*MESH.create_box(face_width, connecting_rod_height, face_width))
-    connecting_rod_position = disk1_position + 0.5 * connecting_rod_height * V3.j() + face_width * V3.k() - eccentricity * V3.j()
+    connecting_rod_position = planet1.position + 0.5 * (planet2.position - planet1.position) + 0.4 * connecting_rod_height * V3.j() - eccentricity * V3.j()
     connecting_rod_orientation = Q.identity()
-    connecting_rod_name = add_object("connecting_rod", connecting_rod_mesh, connecting_rod_position, connecting_rod_orientation, "free")
+    connecting_rod_name = API.add_object(engine, "connecting_rod", connecting_rod_mesh, connecting_rod_position, connecting_rod_orientation, material_name=material_name, density=density)
     
     # Add the ground
     ground_mesh = MESH.Mesh(*MESH.create_box(100, 1, 100))
     ground_position = V3.make(0, -2.2 * ring_spec.rp, 0)
     ground_orientation = Q.identity()
-    ground_name = add_object("ground", ground_mesh, ground_position, ground_orientation, "fixed")
+    ground_name = API.add_object(engine, "ground", ground_mesh, ground_position, ground_orientation, "fixed", material_name, density)
 
     # Create hinge joint between the ground and the crank shaft
-    add_hinge(ground_name, crank_shaft_name, V3.k(), crank_shaft_position)
+    API.add_hinge(ground_name, crank_shaft_name, crank_shaft_position, V3.k())
     
     # Create hinge joint between the crank shaft and the planet gears
     origin = planet1.position + 0.5 * (planet2.position - planet1.position)
-    add_hinge(crank_shaft_name, planet1_name, V3.k(), origin)
-    add_hinge(crank_shaft_name, planet2_name, V3.k(), origin)
-
-    # Create hinge joint between the planet gears and the intermediate disks
-    origin1 = planet1.position
-    origin2 = planet2.position
-    add_hinge(planet1_name, disk1_name, V3.k(), origin1)
-    add_hinge(planet2_name, disk2_name, V3.k(), origin2)
+    API.add_hinge(crank_shaft_name, planet1_name, origin, V3.k())
+    API.add_hinge(crank_shaft_name, planet2_name, origin, V3.k())
     
-    # Create hinge joint between the intermediate disks and the connecting rod
-    origin1 = disk1_position
-    origin2 = disk2_position
-    add_hinge(disk1_name, connecting_rod_name, V3.k(), origin1)
-    add_hinge(disk2_name, connecting_rod_name, V3.k(), origin2)
+    # Create hinge joint between the planet gears and the connecting rod
+    origin1 = planet1.position + face_width * V3.k() - eccentricity * V3.j()
+    origin2 = planet2.position - face_width * V3.k() - eccentricity * V3.j()
+    API.add_hinge(planet1_name, connecting_rod_name, origin1, V3.k())
+    API.add_hinge(planet2_name, connecting_rod_name, origin2, V3.k())
